edf9defd537720d1cc2309446e71f55e21846918
Marcos Santos <marcos.santos@viewnext.com>
2024-05-15 15:01:27 +0200

    Apliacicion CountryApp finalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     AppRoutingModule,
     BrowserModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
